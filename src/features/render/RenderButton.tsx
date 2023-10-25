3665f082643536296b7b6a6ef14dfab21d917ea6
Nick Arthur <nickarthur8@gmail.com>
2023-10-25 16:25:23 -0500

    Add a github link

--- src/features/render/RenderButton.tsx
+++ src/features/render/RenderButton.tsx
@@ -162,11 +162,13 @@ function FillRange() {
   };
 
   return (
-    <div style={{
-      display: 'inline-flex',
-      flexFlow: 'column',
-      alignItems: 'center'
-    }}>
+    <div
+      style={{
+        display: "inline-flex",
+        flexFlow: "column",
+        alignItems: "center",
+      }}
+    >
       <input
         type="range"
         value={value}
@@ -174,11 +176,9 @@ function FillRange() {
         max={0.5}
         step={0.05}
         onChange={handleFillRangeChange}
-        style={{width: '4vw'}}
+        style={{ width: "4vw" }}
       ></input>
-    <label>
-      Aspect Tol {`${value.toFixed(2)}`}      
-    </label>
+      <label>Aspect Tol {`${value.toFixed(2)}`}</label>
     </div>
   );
 }
@@ -323,7 +323,6 @@ function ImportButton() {
   );
 }
 
-
 function AddPhotosButtononClick() {
   const input = document.createElement("input");
   input.type = "file";
@@ -334,7 +333,7 @@ function AddPhotosButtononClick() {
     if (!event.target || !event.target.files) return;
     loadFiles(event.target.files);
     input.remove();
-  };  
+  };
   input.click();
 }
 
@@ -436,6 +435,23 @@ function ResolutionSettings() {
   );
 }
 
+function GithubButton() {
+  return (
+    <>
+      <a href="https://github.com/KnicksKnacks/griddlemypictures" style={{margin: '10px 0'}}>
+        <svg viewBox="0 0 98 96" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+            fill="#24292f"
+          />
+        </svg>
+      </a>
+    </>
+  );
+}
+
 function LoadingBar() {
   const photos = useAppSelector(selectAllPoolPhotos);
   const total = photos.length;
@@ -491,6 +507,7 @@ export function AppButtons() {
       </fieldset>
       <ResolutionSettings />
       <ClearAllButton />
+      <GithubButton/>
       {/* <LinkGooglePhotos /> */}
       <LoadingBar />
     </div>
